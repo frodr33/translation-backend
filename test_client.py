bccdfd1a7520294cca38522f895023f7230a1567
frodr33 <fsr32@cornell.edu>
2020-03-09 17:40:47 -0400

    debugging

--- test_client.py
+++ test_client.py
@@ -4,29 +4,29 @@ import socketio
 import websockets
 import asyncio
 
-# sio = socketio.Client()
-# sio.connect('ws://translation-backend.herokuapp.com/receive ')
-# print('my session identifier (sid) is', sio.sid)
-#
-# sio.emit('join', {"username": "Frank", "sid": sio.sid})
-#
-#
-# @sio.event
-# def message(data):
-#     print(data)
-#
-#
-# while True:
-#     inp = input(">")
-#     sio.emit("message", inp)
+sio = socketio.Client()
+sio.connect('ws://translation-backend.herokuapp.com/receive')
+print('my session identifier (sid) is', sio.sid)
+
+sio.emit('hello')
 
 
-async def hello():
-    uri = "ws://translation-backend.herokuapp.com/receive"
-    async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
+@sio.event
+def message(data):
+    print(data)
 
-        await websocket.send(name)
-        print(f"> {name}")
 
-asyncio.get_event_loop().run_until_complete(hello())
+while True:
+    inp = input(">")
+    sio.emit("message", inp)
+
+
+# async def hello():
+#     uri = "ws://translation-backend.herokuapp.com/receive"
+#     async with websockets.connect(uri) as websocket:
+#         name = input("What's your name? ")
+#
+#         await websocket.send(name)
+#         print(f"> {name}")
+#
+# asyncio.get_event_loop().run_until_complete(hello())
