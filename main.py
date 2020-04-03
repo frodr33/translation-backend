9d39230d9102329289c146c95d42252a26967c48
frodr33 <fsr32@cornell.edu>
2020-04-03 00:05:02 -0400

    adding requests import

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sockets import Sockets
 from flask_socketio import SocketIO, emit, send, join_room
