a0e7250a99daf9b09181d25bdf4f2ccde9ed3301
frodr33 <fsr32@cornell.edu>
2020-05-07 22:19:15 -0400

    print statement to check if old client

--- main.py
+++ main.py
@@ -170,6 +170,9 @@ class ChatBackend:
                         buffered_data = redis.lindex(buffer_key, -1)
                         gevent.spawn(self.send, client, user_id, buffered_data)
 
+                    if str(client) != redis.get(uesr_id + "_client"):
+                        print("Client NOT UP TO DATE")
+
                     # Put this data in buffer
                     buffer_key = user_id + "_buffer"
                     redis.lpush(buffer_key, data)
